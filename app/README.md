51fd469b371a7b2f10a0fa2b2b9d34688360568c
David Aydin <david.aydin6@gmail.com>
2024-10-08 18:28:13 -0400

    Created Password Generator App (#1)
    * npx create-react-app app && project structure setup
    * Removed unused CSS, updated imports, added comments
    * Refactor App.js to update header, main, and footer structure
    * Added ESLint & configuration and update to fix errors/warnings
    * Deployed to GH-Pages
    * Update readme
    * Refactor Repo README.md

---
+++ app/README.md
@@ -0,0 +1,27 @@
+# ChangeLog
+
+### Commit History :
+
+---
+
+### [0.2.0] - 2024-10-XX
+
+- `Pull Request` :
+  - Version Bump
+
+---
+
+### [0.1.0] - 2024-10-08
+
+- `Pull Request` : Created Password Generator App
+  - Refactor Repo README.md
+  - Update README.md
+  - Deployed to GH-Pages
+  - Added ESLint & configuration and update to fix errors/warnings
+  - Refactor App.js to update header, main, and footer structure
+  - Removed unused CSS, updated imports, added comments
+  - npx create-react-app app && project structure setup
+
+---
+
+- First Commit
---
+++ app/src/App.js
@@ -0,0 +1,37 @@
+// Dependencies
+import React from 'react';
+
+// Styles
+import './assets/styles/App.css';
+
+function App() {
+  const companyName = 'daydin14';
+  const year = new Date().getFullYear();
+  const rights = 'All rights reserved.';
+  const author = 'David Aydin';
+
+  return (
+    <div basename='/password-generator'>
+      {/* Header */}
+      <header>
+        <h1>Dave&apos;s PG</h1>
+        <nav>
+          <ul>
+            <li>Home</li>
+          </ul>
+        </nav>
+      </header>
+      {/* Main */}
+      <main>
+        <h1>Hello World</h1>
+      </main>
+      {/* Footer */}
+      <footer>
+        <p>{companyName} &copy; {year} {rights}</p>
+        <p>Created by {author}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
